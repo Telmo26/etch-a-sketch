ae83ad67d460599e9994f37a272fcda6af9b5320
Tadros Eliott <eliott.tadros@student-cs.fr>
2025-05-06 17:26:37 +0200

    Added a button The grid generates corrctly, and now there's a button that will soon have functionality

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-const container = document.querySelector(".container")
-
-let numberOfCells = 8;
-
+let numberOfCells = 16;
 let border = 2 // Size of the border in pixels
 let margin = 4 // Size of the margin in pixels
+    
 
-console.log(innerWidth, innerHeight)
+function createGrid() {
+    const container = document.querySelector(".container")
+    const rect = container.getBoundingClientRect()
+    
+    let containerWidth = rect.width
+    let containerHeight = rect.height
 
-let height = (innerHeight - numberOfCells * 2 * border - numberOfCells * margin) / numberOfCells
-let width = (innerWidth - numberOfCells * 2 * border - numberOfCells * margin) / numberOfCells
-console.log(width, height)
-
-let sizePercentage = Math.round(Math.min(width, height) / Math.min(innerWidth, innerHeight) * 100)
-let size = Math.min(width, height)
-console.log(size)
-
-console.log(Math.min(innerHeight, innerWidth) - numberOfCells * size );
-
-for (let i = 0 ; i < numberOfCells**2 ; i++) {
-    const div = document.createElement("div");
-    div.classList.toggle("child");
-    div.addEventListener("mouseover", () => {
-        div.style.backgroundColor = "red";
-    })
-    // div.style.flexBasis = `${sizePercentage}%`;
-    div.style.height = `${size}px`
-    div.style.width = `${size}px`
-    container.appendChild(div);
+    let height = (containerHeight - numberOfCells * 2 * border - numberOfCells * margin) / numberOfCells
+    let width = (containerWidth - numberOfCells * 2 * border - numberOfCells * margin) / numberOfCells
+    
+    let size = Math.min(width, height)
+    
+    for (let i = 0 ; i < numberOfCells**2 ; i++) {
+        const div = document.createElement("div");
+        div.classList.toggle("child");
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = "red";
+        })
+        div.style.flexBasis = `${size}px`
+        div.style.height = `${size}px`
+        container.appendChild(div);
+    }
 }
+
+createGrid()
